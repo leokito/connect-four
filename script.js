eb247a142ee0568bbe13f87897b0f6b9c7b27d49
Guilherme Job <guilhermejob047@gmail.com>
2021-06-07 14:29:31 -0300

    Criada a tabela via JS, adicionado uma div no html com o id de containerJogo onde sera gerada a tabela

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
 // Construir tabela 7x6 
+// Construir tabela 7x6 
+
+const map = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+]
+let idLinha = 0
+let cont = 0
+for(let i = 0 ; i < 7 ; i++){
+    let linha = document.createElement('div')
+    linha.className = 'linha'
+    linha.id = 'linha' + idLinha
+    idLinha ++
+    linha.style.border = '1px solid red'
+    linha.style.height = '300px'
+    linha.style.width = '50px'
+    let destino = document.getElementById('containerJogo')
+    destino.appendChild(linha)
+    for (let j = 0 ; j < 6; j++){
+        let bloco = document.createElement('div')
+        bloco.style.width = '48px'
+        bloco.style.height = '48px'
+        bloco.style.border = '1px solid green'
+        bloco.className = 'bloco'
+        let destino = document.getElementById('linha' + cont)
+        destino.appendChild(bloco)
+    }
+    cont++
+}
+
+// Colocar disco na tabela ao comando
+
+// Alternar entre os jogadores
+
+// Checar condição de vitória
 
 // Colocar disco na tabela ao comando
 
