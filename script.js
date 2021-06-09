53d8ae8bd7cf4bc916c4cf57bf0b74869cc3190f
David <david.avanci@gmail.com>
2021-06-09 11:09:23 -0300

    pequena refatoração de codigo

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Alternar entre os jogadores
 
-let nome1 = "";
-let nome2 = "";
+let nome1 = "Jogador 1";
+let nome2 = "Jogador 2";
 
 const botao = document.getElementById("submit");
 botao.addEventListener("click", (pegarNome) => {
@@ -14,20 +14,13 @@ botao.addEventListener("click", (pegarNome) => {
 // Construir tabela 7x6
 
 const map = [[], [], [], [], [], [], []];
-let idLinha = 0;
-let cont = 0;
+let destino = document.getElementById("containerJogo");
+
 for (let i = 0; i < 7; i++) {
     let linha = document.createElement("div");
     linha.className = "linha";
-    linha.id = "linha" + idLinha;
-    idLinha++;
-    linha.style.border = "1px solid red";
-    linha.style.height = "300px";
-    linha.style.width = "50px";
-    let destino = document.getElementById("containerJogo");
+    linha.id = "linha" + i;
     destino.appendChild(linha);
-
-    cont++;
 }
 
 // Colocar disco na map ao comando
@@ -41,21 +34,17 @@ linhaId = document.querySelectorAll(".linha").forEach((linha1) =>
         if (linha1.childElementCount < 6) {
             let disco = document.createElement("div");
             let coluna = linha1.id.substr(linha1.id.length - 1);
-            disco.style.width = "48px";
-            disco.style.height = "48px";
+            disco.classList.add('disco');
 
             if (jogador1) {
-                disco.style.background = "black";
                 disco.classList.add("preto");
                 map[coluna].push("P");
                 player.innerText = frases() + nome2;
             } else {
-                disco.style.background = "red";
                 disco.classList.add("vermelho");
                 map[coluna].push("V");
                 player.innerText = frases() + nome1;
             }
-            disco.style.borderRadius = "100%";
             linha1.appendChild(disco);
         }
         checkHorizontal();
@@ -91,11 +80,7 @@ const checkHorizontal = () => {
     for (let i = 0; i < map.length - 3; i++) {
         for (let j = 0; j < map[i].length; j++) {
             let cell = map[i][j];
-            if (
-                cell === map[i + 1][j] &&
-                cell === map[i + 2][j] &&
-                cell === map[i + 3][j]
-            ) {
+            if (cell === map[i + 1][j] && cell === map[i + 2][j] && cell === map[i + 3][j]) {
                 console.log("i = " + i + " / j = " + j);
                 if (jogador1) {
                     console.log("Jogador 1: Vitória horizontal!");
@@ -111,11 +96,7 @@ const checkVertical = () => {
     for (let i = 0; i < map.length; i++) {
         for (let j = 0; j < map[i].length - 3; j++) {
             let cell = map[i][j];
-            if (
-                cell === map[i][j + 1] &&
-                cell === map[i][j + 2] &&
-                cell === map[i][j + 3]
-            ) {
+            if (cell === map[i][j + 1] && cell === map[i][j + 2] && cell === map[i][j + 3]) {
                 if (jogador1) {
                     console.log("Jogador 1: Vitória vertical!");
                 } else {
@@ -131,11 +112,7 @@ const checkDiagonais = () => {
         for (let j = 0; j < 3; j++) {
             let cell = map[i][j];
             if (cell !== undefined) {
-                if (
-                    cell === map[i + 1][j + 1] &&
-                    cell === map[i + 2][j + 2] &&
-                    cell === map[i + 3][j + 3]
-                ) {
+                if (cell === map[i + 1][j + 1] && cell === map[i + 2][j + 2] && cell === map[i + 3][j + 3]) {
                     if (jogador1) {
                         console.log("Jogador 1: Vitória diagonal esquerda!");
                     } else {
@@ -149,11 +126,7 @@ const checkDiagonais = () => {
         for (let j = 0; j < 3; j++) {
             let cell = map[i][j];
             if (cell !== undefined) {
-                if (
-                    cell === map[i - 1][j + 1] &&
-                    cell === map[i - 2][j + 2] &&
-                    cell === map[i - 3][j + 3]
-                ) {
+                if (cell === map[i - 1][j + 1] && cell === map[i - 2][j + 2] && cell === map[i - 3][j + 3]) {
                     if (jogador1) {
                         console.log("Jogador 1: Vitória diagonal direita!");
                     } else {
